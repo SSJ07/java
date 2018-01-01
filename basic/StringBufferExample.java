b4c3f855371032c72a365e57469bddd42ae0f0c9
root <ShrikantJagtap@github.com>
2018-01-02 02:17:35 +0530

    StringBuffer example

---
+++ basic/StringBufferExample.java
@@ -0,0 +1,8 @@
+public class StringBufferExample{
+	public static void main(String args[]){
+		StringBuffer sb = new StringBuffer();
+		sb.append("First statement");
+		sb.append("Second statement");
+		System.out.println(sb);
+	}
+}
