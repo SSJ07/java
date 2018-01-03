cf628f22473d9958e70a872218b720d8eabdd2c6
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-04 04:24:46 +0530

    Add LinkedList Example

---
+++ CollectionDemo/src/org/shri/list/LinkedListExample.java
@@ -0,0 +1,17 @@
+package org.shri.list;
+
+import java.util.LinkedList;
+
+public class LinkedListExample {
+
+	public static void main(String[] args) {
+
+		LinkedList<String> lList = new LinkedList<String>();
+		lList.add("Sachin");
+		lList.add("Yuvraj");
+		lList.add("Dhoni");
+		
+		System.out.println(lList);
+	}
+
+}
