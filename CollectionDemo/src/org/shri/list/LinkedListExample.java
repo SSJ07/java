ea1c6896ea13b501a5685104612f28f1d5fd5a11
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-04 04:38:36 +0530

    LinkedList Example

--- CollectionDemo/src/org/shri/list/LinkedListExample.java
+++ CollectionDemo/src/org/shri/list/LinkedListExample.java
@@ -6,12 +6,24 @@ public class LinkedListExample {
 
 	public static void main(String[] args) {
 
+		/**
+		 * Some points in linked list:
+		 * 1. Best choice to use linked list when insertion and deletion operations frequently
+		 * 2. Worst choice frequently retrieval operation
+		 * 3. Insertion order preserved
+		 * 4. Implements Serializable, Clonable and Dqueue interfaces
+		 * 5. Underlying data structure used is Doubly LinkedList
+		 */
 		LinkedList<String> lList = new LinkedList<String>();
 		lList.add("Sachin");
 		lList.add("Yuvraj");
 		lList.add("Dhoni");
 		
+		
 		System.out.println(lList);
+		System.out.println("Get First Element :" + lList.getFirst());
+		System.out.println("Get Last Element  :" + lList.getLast());
+		System.out.println("Offer element to lList :" + lList.offer("Pandya"));
 	}
 
 }
