cf574fc45be0d2a20a2fe625bfc32b341a5d7e43
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-04 04:45:09 +0530

    Vector Example

---
+++ CollectionDemo/src/org/shri/list/VectorExample.java
@@ -0,0 +1,25 @@
+package org.shri.list;
+
+import java.util.Vector;
+
+public class VectorExample {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Vector is legacy class. Added in jdk1.0
+		 * Vector is same as ArrayList But only difference is all methods of vectors
+		 * are synchronized.
+		 * So vector is thread safe.
+		 */
+		
+		Vector vector = new Vector();
+		vector.add("spring");
+		vector.add(100);
+		System.out.println(vector);
+		System.out.println("size of vector is :" + vector.size());
+		
+		
+	}
+
+}
