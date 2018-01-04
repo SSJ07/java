d879cd76d050077a7fd2189bafde44b6b5d9868b
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-04 23:38:32 +0530

    Create HashMap Example

--- CollectionDemo/src/org/shri/map/HashMapExample.java
+++ CollectionDemo/src/org/shri/map/HashMapExample.java
@@ -1,9 +1,20 @@
 package org.shri.map;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+
 public class HashMapExample {
 
 	public static void main(String[] args) {
-
+		
+		HashMap<String, Integer> hm = new HashMap<String, Integer>();
+		hm.put("Java", 90);
+		hm.put("Python", 95);
+		hm.put("Spring", 89);
+		
+		for(Entry entry : hm.entrySet()){
+			System.out.println(entry.getKey() + ": " + entry.getValue());
+		}
 	}
 
 }
