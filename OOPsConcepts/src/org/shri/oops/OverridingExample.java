5f3d57650c125c616f41d15da267ea1d09033ed6
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-06 23:29:34 +0530

    Method overriding with co-variant

--- OOPsConcepts/src/org/shri/oops/OverridingExample.java
+++ OOPsConcepts/src/org/shri/oops/OverridingExample.java
@@ -15,10 +15,19 @@ class Parent1{
 	public void show(){
 		System.out.println("Showing parent class method");
 	}
+	
+	public Object display(){
+		System.out.println("parent class display method :");
+		return null;
+	}
 }
 class Child1 extends Parent1{
 	public void show(){
 		System.out.println("Showing child class method");
+	}
+	public String display(){
+		System.out.println("child class display method :");
+		return null;
 	}
 }
 public class OverridingExample {
@@ -30,19 +39,26 @@ public class OverridingExample {
 		 */
 		Parent1 parent = new Parent1();
 		parent.show();
+		parent.display();
 		
 		/**
 		 * Child class reference with child class object will call child method.
 		 */
 		Child1 child = new Child1();
 		child.show();
+		child.display();
 		
 		/**
 		 * Parent class reference with child class object will call runtime child class method.
 		 * It's called runtime polymorphism. Cause it call runtime method of child class.
+		 * Compiler checks parent has show method or not compile time.
+		 * But at run time jvm check show method overrided by child or not.
 		 */
 		Parent1 pc = new Child1();
 		pc.show();
+		pc.display();
+		
+		
 	}
 
 }
