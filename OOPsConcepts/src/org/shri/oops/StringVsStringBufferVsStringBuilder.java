db94141bf7d87170fa3b349716ab8bbaf54f6a4b
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-07 20:09:08 +0530

    String Vs StringBuffer vs StringBuilder Example

---
+++ OOPsConcepts/src/org/shri/oops/StringVsStringBufferVsStringBuilder.java
@@ -0,0 +1,17 @@
+package org.shri.oops;
+
+/**
+ * @author Shri
+ * String : String is immutable/can't change
+ * StringBuffer : Immutable and synchronized. Thread safe
+ * StringBuilder: Immutable. Not Synchronized. Not Thread safe.
+ *
+ */
+public class StringVsStringBufferVsStringBuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
