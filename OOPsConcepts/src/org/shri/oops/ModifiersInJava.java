9b4132465d67c3f03f14e5de3803fbacdc8fde2b
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-07 22:11:03 +0530

    Modifiers in java

---
+++ OOPsConcepts/src/org/shri/oops/ModifiersInJava.java
@@ -0,0 +1,48 @@
+package org.shri.oops;
+
+/**
+ * 
+ * @author Shri
+ * 
+ *
+ */
+public class ModifiersInJava {
+
+	public static void main(String[] args) {
+		/**
+		 * For Outer classes:
+		 * 		1. default
+		 * 		2. public
+		 * 		3. strictfp
+		 * 		4. abstract
+		 * 		5. final
+		 * 
+		 * For Inner Classes:
+		 * 		1. default
+		 * 		2. public
+		 * 		3. strictfp
+		 * 		4. abstract
+		 * 		5. final
+		 * 		6. protected
+		 * 		7. private
+		 * 		8. static
+		 * 
+		 * For interfaces :
+		 *  	1. default
+		 * 		2. public
+		 * 		3. strictfp
+		 * 		4. abstract
+		 * 
+		 * For methods : except transient and volatile all modifiers are applicable to method.
+		 * 				 
+		 * For variable: abstract, synchronized, native, strictfp are not applicable other than 
+		 * 				all modifiers are applicable to variable
+		 * 
+		 * Blocks modifiers: only static and synchronized
+		 * 
+		 * 
+		 * 
+		 */
+	}
+
+}
