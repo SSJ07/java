e5bc231757354423a98332439fbb9736f6575e60
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-07 20:45:27 +0530

    String vs StringBuffer vs StringBuilder Example

--- OOPsConcepts/src/org/shri/oops/StringVsStringBufferVsStringBuilder.java
+++ OOPsConcepts/src/org/shri/oops/StringVsStringBufferVsStringBuilder.java
@@ -10,8 +10,55 @@ package org.shri.oops;
 public class StringVsStringBufferVsStringBuilder {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/**
+		 * Conclusion:
+		 * 1. If content is fixed and don't want to change then should go for String.
+		 * 2. If context is not fixed and keep on changing but thread safety is required
+		 * 	  then we should go for StringBuffer.
+		 * 3. If context is not fixed and keep on changing but thread safety is not required
+		 * 	  then we should go for StringBuilder
+		 */
 
+		/**
+		 * create object into heap. ex: id=19
+		 */
+		String heapString =  new String("Hello");
+		/**
+		 * Create object in string constant pool. ex: id=23
+		 */
+		String constantPoolString = "Hello";
+		/**
+		 * Here it will not create new object. 
+		 * It will point previous object. ex: id=23
+		 */
+		String constPoolString = "Hello"; 
+		
+		/** 
+		 * It will create new object "Hello Java". ex: id=24
+		 * It will not update existing s object.
+		 */
+		String change_heapString = heapString.concat(" Java");  
+		System.out.println("heapString :" + heapString);
+		System.out.println("change_heapString :" + change_heapString);
+		
+		/**
+		 * It will not create new object. It just modify existing object
+		 * At time one thread can access it.
+		 * Thread safe
+		 */
+		StringBuffer stringBuffer = new StringBuffer("Hello");
+		stringBuffer.append(" StringBuffer");
+		
+		/**
+		 * It will not create new object. It just modify existing object
+		 * Multiple thread can access it.
+		 * Not Thread safe. 
+		 */
+		StringBuilder stringBuilder = new StringBuilder("Hello");
+		stringBuilder.append(" StringBuilder");
+		
+		System.out.println("stringBuffer :" + stringBuffer);
+		System.out.println("stringBuilder :" + stringBuilder);
 	}
 
 }
