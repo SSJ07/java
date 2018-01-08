3afe71d24d34a878c9741989763591f59e0b78fb
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-09 00:31:35 +0530

    Coupling example

---
+++ OOPsConcepts/src/org/shri/oops/CouplingExamle.java
@@ -0,0 +1,20 @@
+package org.shri.oops;
+
+/**
+ * 
+ * @author Shri
+ * 
+ * The degree of depenency of component is called coupling.
+ * If dependency is more then it is called tightly coupling.
+ * If dependency is less then it is called loosly coupling.
+ * 
+ *
+ */
+public class CouplingExamle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
