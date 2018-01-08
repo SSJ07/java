26c5d38fbf9710aa4f8559819c6fc2674846993c
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-09 00:41:53 +0530

    Coupling Example

--- OOPsConcepts/src/org/shri/oops/CouplingExamle.java
+++ OOPsConcepts/src/org/shri/oops/CouplingExamle.java
@@ -8,8 +8,28 @@ package org.shri.oops;
  * If dependency is more then it is called tightly coupling.
  * If dependency is less then it is called loosly coupling.
  * 
+ * Cohesion : 
+ * 			Every component clear well defined functionality.
+ * 			Spring is high cohesion example.
+ * 			Because every component is defined for particular operations.
  *
  */
+interface P{
+	
+}
+class A1{
+	B b = new B(); // this is called tightly coupling.
+	P p;
+	A1(P p){
+		this.p = p; // this is called loosly coupling.
+	}
+}
+class B implements P{
+	
+}
+class C implements P {
+	
+}
 public class CouplingExamle {
 
 	public static void main(String[] args) {
