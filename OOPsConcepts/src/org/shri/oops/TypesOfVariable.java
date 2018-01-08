aa4b628190c2f72f71a60ae29b71810497a9b0fd
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-08 10:03:22 +0530

    Types of variable in java

---
+++ OOPsConcepts/src/org/shri/oops/TypesOfVariable.java
@@ -0,0 +1,21 @@
+package org.shri.oops;
+
+/**
+ * 
+ * @author Shri
+ * In java variables mainly divided into two types:
+ * 		1. primitive data types : int, long double, float
+ * 		2. reference variable :
+ * 			1. instance variable : It creates each for per object
+ * 			2. static variable  : It creates each for per class
+ * 			3. method variable  : Method level.
+ *
+ */
+public class TypesOfVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
