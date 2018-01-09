28a33df0c9213d481bbfd047407f372f5a695909
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-09 23:57:33 +0530

    Static block example

--- OOPsConcepts/src/org/shri/oops/StaticExample.java
+++ OOPsConcepts/src/org/shri/oops/StaticExample.java
@@ -4,28 +4,31 @@ package org.shri.oops;
  * 
  * @author Shri
  * 
+ * Static block execution flow :
+ *  	
+ *  	1. Identification of static variable member from top to bottom
+ *  	2. Execution of static variable assignments and static blocks from  top to bottom
+ *  	3. execution of main method
  *
  */
 public class StaticExample {
 
-	static int i = 10;
-	static{
-		method1();
-		System.out.println("static block 1");
+	static int i = 10; // execution first
+	static{  // execute seconds
+		method1(); 
+		System.out.println("static block 1"); 
 	}
 	
-	public static void main(String[] args) {
-	
+	public static void main(String[] args) { // execute last/finally
 			method1();
 			System.out.println("main method");
-
 	}
 	public static void method1(){
 		System.out.println("method1 :"+j);
 	}
-	static{
+	static{ // execute third
 		System.out.println("static block 2");
 	}
-	static int j=20;
+	static int j=20; // execute fourth
 
 }
