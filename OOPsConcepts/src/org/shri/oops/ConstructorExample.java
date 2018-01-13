ceea7f955a0c2196090ea31d27a119b0a23cfd88
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-13 23:45:01 +0530

    Constructor Example

--- OOPsConcepts/src/org/shri/oops/ConstructorExample.java
+++ OOPsConcepts/src/org/shri/oops/ConstructorExample.java
@@ -1,10 +1,37 @@
 package org.shri.oops;
 
+/**
+ * 
+ * @author Shri
+ * 
+ * Constructor is used to initialize object not to create an object.
+ * Constructor does not support inheritance concept.
+ * You can call parent constructor in child using super keyword.
+ * Constructor can be overloaded.
+ * In every java class has constructor.
+ * Runtime argument promotion is also applicable in constructor overloading.
+ * char -> int -> long -> float -> double
+ *
+ */
 public class ConstructorExample {
 
+	/** 
+	 * default constructor
+	 * If we not create any constructor jvm provide default constructor.
+	 */
+	ConstructorExample(){
+		System.out.println("Default constructor ");
+	}
+	/**
+	 * Constructor overloading.
+	 * We can create constructor with different argument.
+	 */
+	ConstructorExample(int a){
+		System.out.println("parameterized constructor " + a);
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ConstructorExample ce = new ConstructorExample();
+		ConstructorExample ce1 = new ConstructorExample(10);
 	}
 
 }
