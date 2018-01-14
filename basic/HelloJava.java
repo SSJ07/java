4e5fc3f12b2b1dea6c6d1f60cf63b03a9615f270
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-14 12:32:40 +0530

    Restracture folders

--- basic/HelloJava.java
+++
@@ -1,8 +0,0 @@
-/* This is my first java program on git */
-
-
-public class HelloJava{
-	public static void main(String args[]){
-		System.out.println("Welcome to java world");
-	}
-}
--- basic/StringBufferExample.java
+++
@@ -1,8 +0,0 @@
-public class StringBufferExample{
-	public static void main(String args[]){
-		StringBuffer sb = new StringBuffer();
-		sb.append("First statement");
-		sb.append("Second statement");
-		System.out.println(sb);
-	}
-}
