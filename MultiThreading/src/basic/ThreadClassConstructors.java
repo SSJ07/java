211bdc6dd1898cba44fcea015f9396026826fc16
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-18 05:57:09 +0530

    All information about Thread class like constructors, methods and variables

---
+++ MultiThreading/src/basic/ThreadClassConstructors.java
@@ -0,0 +1,45 @@
+package basic;
+
+
+public class ThreadClassConstructors {
+
+	public static void main(String[] args) {
+
+		/**
+		 * Thread class have various constructors as below:
+		 * 
+		 * 	Thread t = new Thread();
+		 * 	Thread t = new Thread(String name);
+		 * 	Thread t = new Thread(Runnable runnable);
+		 * 	Thread t = new Thread(Runnable runnable, String name);
+		 * 	Thread t = new Thread(ThreadGroup tg, String name);
+		 * 	Thread t = new Thread(ThreadGroup tg, Runnable runnable);
+		 * 	Thread t = new Thread(ThreadGroup tg, Runnable runnable, String name);
+		 * 	Thread t = new Thread(ThreadGroup tg, Runnable runnable, String name, long stacksize);
+		 */
+		
+		/**
+		 * Thread class common methods are :
+		 * 		1. start()
+		 * 		2. join()
+		 * 		3. sleep()
+		 * 		4. getName()
+		 * 		5. setName()
+		 * 		6. run()
+		 * 		7. getPriority()	
+		 * 		8. setPriority()
+		 * 		9. stop()
+		 * 	   10. suspend()
+		 * 	   11. resume() and so on..
+		 */
+		
+		/**
+		 * Fields in Thread class are :
+		 * 		1. static int MAX_PRIORITY = 10
+		 * 		2. static int MIN_PRIORITY = 1
+		 * 		3. static int NORM_PRIORITY = 5
+		 */
+
+	}
+
+}
