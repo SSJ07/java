56a0c6f5e3987196c6688255e94a571ae4e56392
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-19 22:02:41 +0530

    Inter thread communication example. Use of wait() and notify() method

---
+++ MultiThreading/src/interthread/InterThreadCommExample.java
@@ -0,0 +1,46 @@
+package interthread;
+
+/**
+ * 
+ * @author Shri
+ * 
+ * Following methods are used to intercommunication of threads.
+ * wait()
+ * notify()
+ * notifyAll()
+ * 
+ * These methods are only methods which releases lock on object.
+ * These methods always calls inside synchronized area.Otherwise will give IllegalMoniterException.
+ * These methods are in Object class not in thread class.
+ * When we call wait(), then it releases lock immediately.
+ * When we call notify(), then it may not release lock immediately.
+ *
+ */
+
+class InterThread extends Thread{
+	int total;
+	public void run(){
+		synchronized(this){
+			for(byte i=0; i<=100; i++){
+				total+=i;
+			}
+			System.out.println("child thread is trying to notify main thread");
+			this.notify();
+		}
+	}
+}
+public class InterThreadCommExample {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		InterThread it = new InterThread();
+		it.start();
+		
+		synchronized(it){
+			System.out.println("Main thread is trying to enter wait state:");
+			it.wait();
+		}
+		System.out.println("Total="+ it.total);
+	}
+
+}
