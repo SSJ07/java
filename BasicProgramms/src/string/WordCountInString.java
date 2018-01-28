6c59dd61adf584a21206857c0d5b1615845a4eaf
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-29 00:06:58 +0530

    Count words in given string

---
+++ BasicProgramms/src/string/WordCountInString.java
@@ -0,0 +1,21 @@
+package string;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Shri
+ *
+ */
+public class WordCountInString {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter string : ");
+		String string = scan.nextLine();
+		System.out.println("Total words in strings are : "+ string.split(" ").length);
+		scan.close();
+	}
+
+}
