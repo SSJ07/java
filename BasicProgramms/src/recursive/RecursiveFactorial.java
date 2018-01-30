a911dcdc66dd16b239b4ec7993ea0df4fd468149
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-30 12:27:01 +0530

    Finda factorial number using recursion

---
+++ BasicProgramms/src/recursive/RecursiveFactorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+/**
+ * 
+ * @author shrikant
+ * 
+ * Find factorial number using recursive function
+ *
+ */
+public class RecursiveFactorial {
+
+	public static int factorial(int x, int fact){
+		if(x==0 || x==1)
+			return fact;
+		else
+			fact*=x;
+			return factorial(x-=1, fact);
+	}
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Number : ");
+		int x = sc.nextInt();
+		sc.close();
+		System.out.println("factorial of given number is : " + factorial(x, 1));
+		
+	}
+
+}
