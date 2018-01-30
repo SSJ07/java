3048dd403c58038ef9bb167043dbfb2ef00cb853
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-01-30 12:35:45 +0530

    sum of digit using recursion
    Calculate sum of digit in given number using recursion

---
+++ BasicProgramms/src/recursive/SumOfDigit.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author shrikant
+ * 
+ * Find sum of each digit in given number using recursion
+ *
+ */
+public class SumOfDigit {
+
+	public static int sumOfDigit(int num, int sum){
+		if(num==0)
+			return sum;
+		else
+			sum+=num%10;
+			return sumOfDigit(num/=10, sum);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Number : ");
+		int x = sc.nextInt();
+		sc.close();
+		System.out.println("sum of digit in given number is : " + sumOfDigit(x,0));
+	}
+
+}
