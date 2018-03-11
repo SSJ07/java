a1ca408ca7fd0715f9c67571c085aea0e2200af5
ShrikantJagtap <jagtap.shrikant91@gmail.com>
2018-03-11 23:30:19 +0530

    Arrays class example

--- Collection/src/utility/ArraysClassExample.java
+++ Collection/src/utility/ArraysClassExample.java
@@ -17,6 +17,37 @@ public class ArraysClassExample {
 		 */
 		Arrays.sort(array);
 		System.out.println("After sorting array :" + Arrays.toString(array));
+		
+		/**
+		 * Normally Arrays.toString(int [] arr) print outer array elements.
+		 * But deepToString() method prints all array(array inside array) into string format.
+		 * Below line output will be : array deep toString :[1, 2, 3, [0, 1, 2, 3, 4, 5, 6]]
+		 */
+		Object [] arr = {1,2,3,array};
+		System.out.println("array deep toString :" + Arrays.deepToString(arr));
+		
+		/**
+		 * Compare two array object.
+		 * Arrays.equals(int [], int[]) This required both sorted array as input
+		 */
+		
+		int a[] = {1,2,3,0};
+		int b[] = {1,2,3,0};
+		System.out.println("Is array object equals? : " + Arrays.equals(a,b));
+		
+		/**
+		 * make copy of array
+		 */
+	
+		int [] c = Arrays.copyOf(a, a.length);
+		if( c==a){
+			System.out.println("Array a==c refering same reference");
+		}
+		if(Arrays.equals(a, c)){
+			System.out.println("Array a and c are same");
+		}
 	}
+	
+	
 
 }
